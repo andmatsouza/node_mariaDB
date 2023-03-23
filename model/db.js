7b8cc7fbfbe2640877b171ed820f418fa95d1a3c
andmatsou <andmatsou@gmail.com>
2023-03-23 14:23:05 -0300

    adicionado comentários no db.js

--- model/db.js
+++ model/db.js
@@ -1,11 +1,14 @@
+//importamos o sequelize p conectar ao banco de dados
 const Sequelize = require("sequelize");
 
+//criamos a nossa string de conexão
 const sequelize = new Sequelize('sigap', 'root', '123456', {
   host: 'localhost', 
   dialect: 'mariadb',
   port: '3305'  
 });
 
+//verifica se conectou com sucesso
 sequelize.authenticate()
 .then(() =>{
   console.log('Connection has been established successfully.');
@@ -15,5 +18,5 @@ sequelize.authenticate()
 }) ;
   
 
-
+//exportamos a const Sequelize p usarmos em outros módulos
 module.exports = sequelize;
