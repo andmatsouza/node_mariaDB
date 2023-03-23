3e9505bd35999effe57f4abc6276b7b2c3115497
andmatsou <andmatsou@gmail.com>
2023-03-23 14:28:47 -0300

    adicionado comentários no arquivo user

--- model/User.js
+++ model/User.js
@@ -1,6 +1,8 @@
+//importamos o sequelize 
 const Sequelize = require("sequelize");
+//importamos o módulo de conexão ao banco
 const db = require("./db");
-
+//crianmos a tabela users no banco de dados usando o sequelize
 const User = db.define('users', {
   id:{
     type: Sequelize.INTEGER,
@@ -18,6 +20,8 @@ const User = db.define('users', {
   }
 });
 
+//função do sequelize usada p sincronizar com o banco de dados
 User.sync();
 
+//exportamos a const User p usarmos em outros módulos
 module.exports = User;
